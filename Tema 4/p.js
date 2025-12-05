8ed73a2dd81b90eee1836e56ac760e3995196f5d
EzequielSebas <sebastian.ezequiel.guzman1@gmail.com>
2025-12-05 14:15:07 +0100

    Add files via upload

--- Tema 4/p.js
+++ Tema 4/p.js
@@ -8,3 +8,8 @@ console.log("Es viernes y queremosn irnos: " + esviernes);
 // debugger
 if(esviernes == true)
 {console.log("Pues si que es viernes, nos queda media hora")}
+
+
+var nombre_de_usuario = prompt ("Introduszca su nombre:");
+console.log("El nombre del usuario es: ", nombre_de_usuario)
+document.write("<h1>" + nombre_de_usuario + "</h1>")
