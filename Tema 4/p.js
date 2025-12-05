0d93e84b3eeed6d7a8e8a7939d09db5f99e06e41
EzequielSebas <sebastian.ezequiel.guzman1@gmail.com>
2025-12-05 13:50:57 +0100

    Add files via upload

---
+++ Tema 4/p.js
@@ -0,0 +1,10 @@
+var mensaje = "hola como estas";
+console.log("Buenas si que tal buenas tardes")
+console.log(mensaje)
+
+
+var esviernes = true
+console.log("Es viernes y queremosn irnos: " + esviernes);
+// debugger
+if(esviernes == true)
+{console.log("Pues si que es viernes, nos queda media hora")}
